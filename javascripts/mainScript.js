e7c97207606bc2c616f00e891302c905850a083c
harsh89 <harshvardhan.sharma@DEL1-DHP-31576.synapse.com>
2015-08-11 12:37:56 +0530

    commit after more js and css changes

--- javascripts/mainScript.js
+++ javascripts/mainScript.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
-
 var currentIndex =0;
 var showAllFlag = false;
 
 var campaignData = '[{"name":"Campaign Name", "dateCreated":"12 Dec 2014", "lastUpdated":"18 Dec 2014","totalViews":"5k","sales":"1000 units","endDate":"31 Dec 2014"},{"name":"Campaign Name", "dateCreated":"12 Dec 2014", "lastUpdated":"18 Dec 2014","totalViews":"5k","sales":"1000 units","endDate":"31 Dec 2014"},{"name":"Campaign Name", "dateCreated":"12 Dec 2014", "lastUpdated":"18 Dec 2014","totalViews":"5k","sales":"1000 units","endDate":"31 Dec 2014"},{"name":"Campaign Name", "dateCreated":"12 Dec 2014", "lastUpdated":"18 Dec 2014","totalViews":"5k","sales":"1000 units","endDate":"31 Dec 2014"},{"name":"Campaign Name", "dateCreated":"12 Dec 2014", "lastUpdated":"18 Dec 2014","totalViews":"5k","sales":"1000 units","endDate":"31 Dec 2014"},{"name":"Campaign Name", "dateCreated":"12 Dec 2014", "lastUpdated":"18 Dec 2014","totalViews":"5k","sales":"1000 units","endDate":"31 Dec 2014"},{"name":"Campaign Name", "dateCreated":"12 Dec 2014", "lastUpdated":"18 Dec 2014","totalViews":"5k","sales":"1000 units","endDate":"31 Dec 2014"},{"name":"Campaign Name", "dateCreated":"12 Dec 2014", "lastUpdated":"18 Dec 2014","totalViews":"5k","sales":"1000 units","endDate":"31 Dec 2014"},{"name":"Campaign Name", "dateCreated":"12 Dec 2014", "lastUpdated":"18 Dec 2014","totalViews":"5k","sales":"1000 units","endDate":"31 Dec 2014"},{"name":"Campaign Name", "dateCreated":"12 Dec 2014", "lastUpdated":"18 Dec 2014","totalViews":"5k","sales":"1000 units","endDate":"31 Dec 2014"},{"name":"Campaign Name", "dateCreated":"12 Dec 2014", "lastUpdated":"18 Dec 2014","totalViews":"5k","sales":"1000 units","endDate":"31 Dec 2014"},{"name":"Campaign Name", "dateCreated":"12 Dec 2014", "lastUpdated":"18 Dec 2014","totalViews":"5k","sales":"1000 units","endDate":"31 Dec 2014"},{"name":"Campaign Name", "dateCreated":"12 Dec 2014", "lastUpdated":"18 Dec 2014","totalViews":"5k","sales":"1000 units","endDate":"31 Dec 2014"},{"name":"Campaign Name", "dateCreated":"12 Dec 2014", "lastUpdated":"18 Dec 2014","totalViews":"5k","sales":"1000 units","endDate":"31 Dec 2014"},{"name":"Campaign Name", "dateCreated":"12 Dec 2014", "lastUpdated":"18 Dec 2014","totalViews":"5k","sales":"1000 units","endDate":"31 Dec 2014"},{"name":"Campaign Name", "dateCreated":"12 Dec 2014", "lastUpdated":"18 Dec 2014","totalViews":"5k","sales":"1000 units","endDate":"31 Dec 2014"},{"name":"Campaign Name", "dateCreated":"12 Dec 2014", "lastUpdated":"18 Dec 2014","totalViews":"5k","sales":"1000 units","endDate":"31 Dec 2014"},{"name":"Campaign Name", "dateCreated":"12 Dec 2014", "lastUpdated":"18 Dec 2014","totalViews":"5k","sales":"1000 units","endDate":"31 Dec 2014"},{"name":"Campaign Name", "dateCreated":"12 Dec 2014", "lastUpdated":"18 Dec 2014","totalViews":"5k","sales":"1000 units","endDate":"31 Dec 2014"},{"name":"Campaign Name", "dateCreated":"12 Dec 2014", "lastUpdated":"18 Dec 2014","totalViews":"5k","sales":"1000 units","endDate":"31 Dec 2014"}]';
 
 var myCampaignData = JSON.parse(campaignData);
+
+$(document).ready(function(){
 
 var initialLoad = function(flag){
 
@@ -60,9 +60,9 @@ for(var i=currentIndex; i<myCampaignData.length ;i++){
 
 }
 
-var showAllElem = document.getElementById("lblShowAll");
+var $showAllElem = document.getElementById("lblShowAll");
 
-showAllElem.addEventListener("click", function(){
+$showAllElem.addEventListener("click", function(){
 
 	if(!showAllFlag){
 	currentIndex = 0;
@@ -71,21 +71,21 @@ showAllElem.addEventListener("click", function(){
 initialLoad(false);
 });
 
-var updateElem = document.getElementById("lblUpdate");
-updateElem.addEventListener("click", function(){
+var $updateElem = document.getElementById("lblUpdate");
+$updateElem.addEventListener("click", function(){
 
 if(currentIndex === myCampaignData.length){
 	alert("Data is already upto date");
 	return;
 }
-var parentNode = document.getElementById("campaignDataTable");
+var $parentNode = document.getElementById("campaignDataTable");
 var oldData = parentNode.getElementsByTagName("ul");
 var oldHrTag = parentNode.getElementsByTagName("hr");
 
 for(x = oldData.length - 1; x >= 0; x--){
 
-	oldData[x].parentNode.removeChild(oldData[x]);
-	oldHrTag[x].parentNode.removeChild(oldHrTag[x]);
+	oldData[x].$parentNode.removeChild(oldData[x]);
+	oldHrTag[x].$parentNode.removeChild(oldHrTag[x]);
 }
 initialLoad(true);
 });
