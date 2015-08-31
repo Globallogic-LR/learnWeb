81c6e620242a757689c4aebd94e2e466d3840d6d
harsh89 <harshvardhan.sharma@DEL1-DHP-31576.synapse.com>
2015-08-31 18:21:18 +0530

    row edit functionality added along with other minor changes

--- javascripts/mainScript.js
+++ javascripts/mainScript.js
@@ -9,25 +9,21 @@ $(document).ready(function(){
 	var editButtonElems = document.getElementsByClassName("fa-pencil");
 	var trashButtonElems = document.getElementsByClassName("fa-trash-o");
 	var faIconArr = [playButtonElems, editButtonElems, trashButtonElems];
+	var inputBoxRows = document.getElementById("mainDataTable").getElementsByTagName('tr');
 	var savedTemplateFunc = (function(){
-		var htmlTemplate ='<tr> \
+		var htmlTemplate ='<tr class="inputbox-visible-class"> \
 		 		<td id="actionCell" class="td-template-class td-first-template"><i id="playButton" class="fa fa-play"></i> <i class="fa fa-pencil"></i> <i class="fa fa-trash-o"></i> </td> \
-		 		<td id="nameCell" class="td-template-class">Name</td> \
-		 		<td id="descriptionCell" class="td-template-class td-description-template">Description</td> \
-		 		<td id="ownerNameCell" class="td-template-class">Owner Name</td> \
-		 		<td id="permissionCell" class="td-template-class">Read Permissions</td> \
+		 		<td id="nameCell" class="td-template-class"><input disabled = "true" id="inputNameCell" type="text" placeholder="Type Here" class="input-table-field"></td> \
+		 		<td id="descriptionCell" class="td-template-class td-description-template"><input disabled = "true" id="inputDescriptionCell" type="text" placeholder="Type Here" class="input-table-field"></td> \
+		 		<td id="ownerNameCell" class="td-template-class"><input disabled = "true" id="inputOwnerNameCell" type="text" placeholder="Type Here" class="input-table-field"></td> \
+		 		<td id="permissionCell" class="td-template-class"><input disabled = "true" id="inputPermissionCell" type="text" placeholder="Type Here" class="input-table-field"></td> \
 		 		</tr>';
 
-		var enableTextboxRow = function (element, rowIndex) {
-/*			console.log("row index = "+rowIndex);
-			console.log("element = "+element);
-			if(element == editButtonElems){
-				console.log("inside");
-			}
-			else
-			{
-				console.log("outside");
-			}*/
+		var enableTextboxRow = function (rowIndex){
+			var inputBox = inputBoxRows[rowIndex-2].getElementsByTagName('input');
+			for(var i=0; i<inputBox.length;i++){
+				inputBox[i].disabled = '';
+			}	
 		}
 
 		var htmlTemplateFunc = function(templateData){
@@ -40,18 +36,18 @@ $(document).ready(function(){
 			
 			for(var i=0; i<templateData.length ;i++){
 				tableElem.insertAdjacentHTML('afterbegin', htmlTemplate);
-				document.getElementById("nameCell").textContent = templateData[i].name;
-				document.getElementById("descriptionCell").textContent = templateData[i].description;
-				document.getElementById("ownerNameCell").textContent = templateData[i].ownerName;
-				document.getElementById("permissionCell").textContent = templateData[i].readPermissions;	
+				document.getElementById('inputNameCell').value = templateData[i].name;
+				document.getElementById("inputDescriptionCell").value = templateData[i].description;
+				document.getElementById("inputOwnerNameCell").value = templateData[i].ownerName;
+				document.getElementById("inputPermissionCell").value = templateData[i].readPermissions;	
 
 				//to be done later for string interpolation
 				//str.replace ('', function(m){ return myCampaignData[i][m]})
 			}
 			for (var i = 0; i <faIconArr.length; i++){
 				for(var j=0; j<faIconArr[i].length; j++){
-					faIconArr[i][j].addEventListener("click", function(e){
-						enableTextboxRow(faIconArr[i], this.parentNode.parentNode.rowIndex);
+					faIconArr[1][j].addEventListener("click", function(e){
+						enableTextboxRow(this.parentNode.parentNode.rowIndex);
 					});
 				};
 			};
@@ -66,5 +62,4 @@ $(document).ready(function(){
 	allSaved.addEventListener("click",function(){savedTemplateFunc.pushAllSavedData()});
 	sharedSearchesElem.addEventListener("click",function(){savedTemplateFunc.pushSharedSearchesData()});
 	savedTemplateFunc.pushAllSavedData();
-
 });
