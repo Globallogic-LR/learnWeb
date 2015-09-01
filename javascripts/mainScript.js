60c98b53afef492f2f9aacfe9f99227e43b29cd6
harsh89 <harshvardhan.sharma@DEL1-DHP-31576.synapse.com>
2015-09-01 18:15:13 +0530

    session storage added and some code optimization

--- javascripts/mainScript.js
+++ javascripts/mainScript.js
@@ -1,18 +1,18 @@
-var campaignData = '[{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"}]';
-var myCampaignData = JSON.parse(campaignData);
+var campaignData = '[{"name":"Campaign Name", "description":" Description", "ownerName":"Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"}]';
+//var myCampaignData = JSON.parse(campaignData);
 var sharedSearchData = '[{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"},{"name":"Campaign Name", "description":" Description", "ownerName":" Owner Name","readPermissions":"private"}]';
-var mysharedSearchData = JSON.parse(sharedSearchData);
+//var mysharedSearchData = JSON.parse(sharedSearchData);
+sessionStorage.setItem("localCampaignData",campaignData);
+var myCampaignData = JSON.parse(sessionStorage.getItem("localCampaignData"));
+sessionStorage.setItem("localSharedSearchData",sharedSearchData);
+var mysharedSearchData = JSON.parse(sessionStorage.getItem("localSharedSearchData"));
 $(document).ready(function(){
 	var tableElem = document.getElementById("mainDataTable");
 	var mainTableBodyElem= document.getElementById("mainTableBody");
-	var playButtonElems = document.getElementsByClassName("fa-play");
-	var editButtonElems = document.getElementsByClassName("fa-pencil");
-	var trashButtonElems = document.getElementsByClassName("fa-trash-o");
-	var faIconArr = [playButtonElems, editButtonElems, trashButtonElems];
 	var inputBoxRows = document.getElementById("mainDataTable").getElementsByTagName('tr');
 	var savedTemplateFunc = (function(){
 		var htmlTemplate ='<tr class="inputbox-visible-class"> \
-		 		<td id="actionCell" class="td-template-class td-first-template"><i id="playButton" class="fa fa-play"></i> <i class="fa fa-pencil"></i> <i class="fa fa-trash-o"></i> </td> \
+		 		<td id="actionCell" class="td-template-class td-first-template"><i id="playButton" class="fa fa-play"></i> <i id="editButton" class="fa fa-pencil"></i> <i class="fa fa-trash-o"></i> </td> \
 		 		<td id="nameCell" class="td-template-class"><input disabled = "true" id="inputNameCell" type="text" placeholder="Type Here" class="input-table-field"></td> \
 		 		<td id="descriptionCell" class="td-template-class td-description-template"><input disabled = "true" id="inputDescriptionCell" type="text" placeholder="Type Here" class="input-table-field"></td> \
 		 		<td id="ownerNameCell" class="td-template-class"><input disabled = "true" id="inputOwnerNameCell" type="text" placeholder="Type Here" class="input-table-field"></td> \
@@ -23,7 +23,16 @@ $(document).ready(function(){
 			var inputBox = inputBoxRows[rowIndex-2].getElementsByTagName('input');
 			for(var i=0; i<inputBox.length;i++){
 				inputBox[i].disabled = '';
-			}	
+			}
+		}
+
+		var saveRowData = function (rowIndex){
+			var inputBox = inputBoxRows[rowIndex-2].getElementsByTagName('input');
+			for(var i=0; i<inputBox.length;i++){
+				console.log(inputBox[i].value);
+				console.log(myCampaignData[i]);
+				inputBox[i].disabled = 'disabled';
+			}
 		}
 
 		var htmlTemplateFunc = function(templateData){
@@ -36,6 +45,12 @@ $(document).ready(function(){
 			
 			for(var i=0; i<templateData.length ;i++){
 				tableElem.insertAdjacentHTML('afterbegin', htmlTemplate);
+				document.getElementById('playButton').addEventListener("click", function(e){
+						saveRowData(this.parentNode.parentNode.rowIndex);
+				});
+				document.getElementById('editButton').addEventListener("click", function(e){
+						enableTextboxRow(this.parentNode.parentNode.rowIndex);
+				});
 				document.getElementById('inputNameCell').value = templateData[i].name;
 				document.getElementById("inputDescriptionCell").value = templateData[i].description;
 				document.getElementById("inputOwnerNameCell").value = templateData[i].ownerName;
@@ -44,13 +59,6 @@ $(document).ready(function(){
 				//to be done later for string interpolation
 				//str.replace ('', function(m){ return myCampaignData[i][m]})
 			}
-			for (var i = 0; i <faIconArr.length; i++){
-				for(var j=0; j<faIconArr[i].length; j++){
-					faIconArr[1][j].addEventListener("click", function(e){
-						enableTextboxRow(this.parentNode.parentNode.rowIndex);
-					});
-				};
-			};
 		};
 		return{
 			pushAllSavedData:function(){htmlTemplateFunc(myCampaignData)},
