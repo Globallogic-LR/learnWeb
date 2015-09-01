06902995ba00ee96be558e57c09d1ac05cddf166
Danish Arora <daniarora.17@gmail.com>
2015-09-01 12:45:01 +0530

    Update main-script.js
    Updated code ..

--- Assignment-screen/main-script.js
+++ Assignment-screen/main-script.js
@@ -1,36 +1,49 @@
-var data =[{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"}];
-
-
-
-
-var newt = document.getElementsByClassName("data-show")[0];
-var items = data;
-
-for(var i = 0; i < items.length; i++) {
-    var newTr = document.createElement("tr");
-
-    var newTd1 = document.createElement('td')
-    var newTd2 = document.createElement('td')
-    var newTd3 = document.createElement('td')
-    var newTd4 = document.createElement('td')
-    var newTd5 = document.createElement('td')
-
-    newTr.appendChild(newTd1)
-    newTr.appendChild(newTd2)
-    newTr.appendChild(newTd3)
-    newTr.appendChild(newTd4)
-    newTr.appendChild(newTd5)
-
-
-
-    newTd1.innerHTML = '<span class="glyphicon glyphicon-play color-gly"></span>   <span class="glyphicon glyphicon-pencil color-gly"></span>   <span class="glyphicon glyphicon-trash color-gly"></span>';
-    newTd2.innerHTML = items[i].name;
-    newTd3.innerHTML = items[i].description;
-    newTd4.innerHTML = items[i].ownername;
-    newTd5.innerHTML = items[i].readpermissions;
-
-	console.log(newTr)
-   	newt.appendChild(newTr);
-
-
-   }
+var data =[{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Testing the save as feature", "description":"TC2 Owner is Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Ad Hoc search edited", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"TC-16 Owner is Restriced", "description":"TC-16 Owner is Restriced ","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"},{"name":"Edited the search Criteria", "description":"TC and Global Admin","ownername":"LogRhythm Admin","readpermissions":"Private"}];
+var data2 =[{"name":"TC 16-Owner is Restricted", "description":"TC 16-owner is Restricted Analyst","ownername":"restrictedanalyst","readpermissions":"Public Restricted Analyst"},{"name":"TC 16-Owner is Restricted", "description":"TC 16-owner is Restricted" ,"ownername":"restrictedanalyst","readpermissions":"Public Restricted Analyst"},{"name":"TC 16-Owner is Restricted", "description":"TC 16-owner is Restricted","ownername":"restrictedanalyst","readpermissions":"Public Restricted Analyst"},{"name":"TC 16-Owner is Restricted", "description":"TC 16-owner is Restricted ","ownername":"restrictedanalyst","readpermissions":"Public Restricted Analyst"},{"name":"TC 16-Owner is Restricted", "description":"TC 16-owner is Restricted","ownername":"restrictedanalyst","readpermissions":"Public Restricted Analyst"},{"name":"TC 16-Owner is Restricted", "description":"TC 16-owner is Restricted","ownername":"restrictedanalyst","readpermissions":"Public Restricted Analyst"},{"name":"TC 16-Owner is Restricted", "description":"TC 16-owner is Restricted","ownername":"restrictedanalyst","readpermissions":"Public Restricted Analyst"},{"name":"TC 16-Owner is Restricted", "description":"TC 16-owner is Restricted","ownername":"restrictedanalyst","readpermissions":"Public Restricted Analyst"},{"name":"TC 16-Owner is Restricted", "description":"TC 16-owner is Restricted","ownername":"restrictedanalyst","readpermissions":"Public Restricted Analyst"},{"name":"TC 16-Owner is Restricted", "description":"TC 16-owner is Restricted","ownername":"restrictedanalyst","readpermissions":"Public Restricted Analyst"},{"name":"TC 16-Owner is Restricted", "description":"TC 16-owner is Restricted","ownername":"restrictedanalyst","readpermissions":"Public Restricted Analyst"},{"name":"TC 16-Owner is Restricted", "description":"TC 16-owner is Restricted","ownername":"restrictedanalyst","readpermissions":"Public Restricted Analyst"},{"name":"TC 16-Owner is Restricted", "description":"TC 16-owner is Restricted","ownername":"restrictedanalyst","readpermissions":"Public Restricted Analyst"},{"name":"TC 16-Owner is Restricted", "description":"TC 16-owner is Restricted","ownername":"restrictedanalyst","readpermissions":"Public Restricted Analyst"},{"name":"TC 16-Owner is Restricted", "description":"TC 16-owner is Restricted","ownername":"restrictedanalyst","readpermissions":"Public Restricted Analyst"},{"name":"TC 16-Owner is Restricted", "description":"TC 16-owner is Restricted","ownername":"restrictedanalyst","readpermissions":"Public Restricted Analyst"}];
+var addTable = document.getElementsByClassName("data-show")[0];
+var gly = '<span class="glyphicon glyphicon-play color-gly"></span><span class="glyphicon glyphicon-pencil color-gly"></span><span class="glyphicon glyphicon-trash color-gly"></span>';
+var winloc = window.location.pathname;
+//console.log(winloc);
+function createTr(j){                       //function to create tr
+  return document.createElement("tr");
+}
+function createTd(i, rowData){                        //function to create td
+  var td = document.createElement("td");
+    switch (i){
+      case 0:
+        td.innerHTML = gly;
+        break;
+      case 1:
+        td.innerHTML = rowData.name;
+        break;
+      case 2:
+        td.innerHTML = rowData.description;
+        break;
+      case 3:
+        td.innerHTML = rowData.ownername;
+        break;
+      case 4:
+        td.innerHTML = rowData.readpermissions;
+        break;
+    }
+    return td;
+}
+var populateData = function(data12){              //function to dynamic make tr with 4 td inside to the data length
+      if (winloc == "/C:/Users/danish.arora/Desktop/danish/screen-1.html") {
+        data12 = data;
+      }
+      else{
+        data12 = data2;
+      }
+      //console.log(data12);
+      for (var j = 0; j < data12.length; j++) {
+      //console.log(data12[j].name);
+      var tr = createTr(j);  
+      for (var i = 0; i <=4; i++) {
+        var td = createTd(i, data12[j]);
+        tr.appendChild(td);
+      }
+      addTable.appendChild(tr);                   
+    }
+  }
+window.load = populateData(data);
